2ee09f3b8ee04240a38d29d293e305930d3294f5
Domonkos Csuzdi <csuzdi02@gmail.com>
2022-11-18 21:37:45 +0100

    add resetProgressMW

--- middleware/user/resetAllUserProgressMW.js
+++ middleware/user/resetAllUserProgressMW.js
@@ -1,11 +1,27 @@
 /**
- * Loads all users from the database belonging to res.locals.game
  * Only runs if the MAJOR version of the game has changed
- * By using POST, resets the progress for all of the users
+ * By using POST, resets the progress for all of the users belonging to res.locals.game
  */
 
 module.exports = function (objectrepository) {
     return function (req, res, next) {
-        next();
+        const old_version = res.locals.game.version;
+        const new_version = req.body.version;
+
+        // do not run for GET
+        if (typeof new_version === 'undefined') {
+            return next();
+        }
+
+        const old_major_version = old_version.split('.')[0];
+        const new_major_version = new_version.split('.')[0];
+
+        if (old_major_version !== new_major_version) {
+            res.locals.users.forEach((user) => {
+                user.progress = 0;
+                user.save();
+            });
+        }
+        return next();
     };
 };
